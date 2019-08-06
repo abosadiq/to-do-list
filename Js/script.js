60e8e1aad50b63a2efe3aa1e43a57fa19d954a62
wafi mohamed <wafimohamed@wafis-MacBook-Pro.local>
2019-08-06 16:57:52 -0700

    done

---
+++ Js/script.js
@@ -0,0 +1,42 @@
+// Check
+$(document).ready(function() {
+  $("ul").on("click", "li", function() {
+    if ($("li").css("color") === "rgb(128, 128, 128)") {
+      $(this).css({
+        color: "black",
+        textDecoration: "none"
+      });
+    } else {
+      $(this).css({
+        color: "gray",
+        textDecoration: "line-through"
+      });
+    }
+  });
+  // delete list
+  $("ul").on("click", ".delete", function(e) {
+    e.stopPropagation();
+    $(this)
+      .parent()
+      .fadeOut(function() {
+        $(this).remove();
+      });
+  });
+  $(".add-list").on("keypress", function(e) {
+    if (e.which === 13) {
+      let addedText = $(this).val();
+
+      // to remove the text from input text after we hit eneter
+      $(this).val("");
+      // make a new li and add it to ul
+      $("ul").append(
+        `<li><svg class='delete'><use xlink:href="../img/sprite.svg#icon-bin"></use></svg> ${addedText}</li>`
+      );
+    }
+  });
+
+  // hiding the text input when click in + button
+  $(".heading__btn").on("click", function() {
+    $(".add-list").fadeToggle(500);
+  });
+});
